31afc2a8ede4f641b35df87b66a9d7c9adaf16ab
mahboob <mahboob65590@gmail.com>
2025-10-05 14:10:58 +0530

    added one more route

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/',(req,res)=>{
     res.send("hello world");
 })
 
+app.get('/login',(req,res)=>{
+    res.send("<h1>welcome to login page</h1>");
+})
+
 app.listen(port,()=>{
     console.log("port listening at ", port);
 })
